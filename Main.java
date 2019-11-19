2dc2362c6f7c5152022da300b5901a5549cae1df
Alan <nickrodness@gmail.com>
2019-11-19 23:01:51 +0600

    added comments to the solutions

--- Main.java
+++ Main.java
@@ -9,20 +9,36 @@ public class Main {
     int[][] input_route_length = new int[N][N];
     long[][][] path_count = new long[N][N][max_len + 1];
     long[][][] path_count_with_len = new long[N][N][max_len + 1];
+    long[] answer = new long[11];
 
-    public void solve() {
-		Scanner scanner = null;
-        try {
-			scanner = new Scanner(new FileReader("test_input"));
-        } catch (Exception e) {
-			System.out.println("No input file");
-			return;
-		}
-		for (int vertex_from = 0; vertex_from < N; ++vertex_from)
+    /*
+        this function initializes initial states for each of the dynamic programming arrays
+     */
+    private void init() {
+        long[][][] path_count = new long[N][N][max_len + 1];
+        long[][][] path_count_with_len = new long[N][N][max_len + 1];
+        for (int vertex_from = 0; vertex_from < N; ++vertex_from)
             for (int vertex_to = 0; vertex_to < N; ++vertex_to) {
                 route_length[vertex_from][vertex_to] = 1000 * 1000 * 1000;
                 input_route_length[vertex_from][vertex_to] = 1000 * 1000 * 1000;
             }
+    }
+
+    /*
+        this function reads input file and build the graph
+
+        closes the program if there is no input file or it has wrong format
+
+     */
+    private void read() {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new FileReader("test_input"));
+        } catch (Exception e) {
+            System.out.println("No input file");
+            System.exit(0);
+        }
+
         while (scanner.hasNext()) {
             String edge = null;
             try {
@@ -37,8 +53,19 @@ public class Main {
                 path_count_with_len[vertex_from][vertex_to][edge_length] = 1;
             } catch (Exception e) {
                 System.out.println("You have entered wrong input format, try to enter it again");
+                System.exit(0);
             }
         }
+    }
+
+    /*
+        this function calculates shortest path between each pair of vertices
+
+        in result 2-dimensional array route_length is calculated
+
+        return: null
+     */
+    private void calculate_shortest_path() {
         for (int intermediate_vertex = 0; intermediate_vertex < N; ++intermediate_vertex)
             for (int vertex_from = 0; vertex_from < N; ++vertex_from)
                 for (int vertex_to = 0; vertex_to < N; ++vertex_to)
@@ -48,6 +75,17 @@ public class Main {
                                 route_length[vertex_from][vertex_to],
                                 route_length[vertex_from][intermediate_vertex] + route_length[intermediate_vertex][vertex_to]
                         );
+    }
+
+    /*
+        this function calculate number of paths between each pair of vertices
+        for each fixed number of stops from 0 to 30
+
+        in result 3-dimensional array path_count is calculated
+
+        return: null
+     */
+    private void calculate_number_of_paths_stops() {
         for (int current_route_length = 0; current_route_length < 30; ++current_route_length) {
             for (int vertex_from = 0; vertex_from < N; ++vertex_from)
                 for (int vertex_to = 0; vertex_to < N; ++vertex_to) {
@@ -58,7 +96,17 @@ public class Main {
                     }
                 }
         }
+    }
 
+    /*
+        this function calculate number of paths between each pair of vertices
+        for each fixed length of the path from 0 to 30
+
+        in result 3-dimensional array path_count_with_len is calculated
+
+        return: null
+     */
+    private void calculate_number_of_paths_length() {
         for (int current_route_length = 0; current_route_length < 30; ++current_route_length) {
             for (int vertex_from = 0; vertex_from < N; ++vertex_from)
                 for (int vertex_to = 0; vertex_to < N; ++vertex_to) {
@@ -70,9 +118,9 @@ public class Main {
                     }
                 }
         }
+    }
 
-        long[] answer = new long[11];
-
+    private void calculate_answers() {
         answer[1] = input_route_length[0][1] + input_route_length[1][2];
         answer[2] = input_route_length[0][3];
         answer[3] = input_route_length[0][3] + input_route_length[3][2];
@@ -84,10 +132,13 @@ public class Main {
         answer[9] = route_length[1][1];
         answer[10] = 0;
 
-
         for (int len = 0; len < 30; ++len) {
             answer[10] += path_count_with_len[2][2][len];
         }
+    }
+
+
+    private void print_answers() {
         for (int output = 1; output <= 10; ++output) {
             System.out.print("Output #" + Integer.toString(output) + ": ");
             if ((output <= 5 || output == 8 || output == 9) && answer[output] >= 1000 * 1000 * 1000) {
@@ -96,6 +147,20 @@ public class Main {
                 System.out.println(answer[output]);
             }
         }
+    }
+
+    public void solve() {
+
+        init();
+        read();
+
+        calculate_shortest_path();
+        calculate_number_of_paths_stops();
+        calculate_number_of_paths_length();
+
+        calculate_answers();
+        print_answers();
+
     }
 
     public static void main(String[] args) {
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 java test task for digital lab
+
+
+instruction:
+	modify input file "test_input" to test
+	build docker container with following command:
+		sudo docker build -t java_test .
+
+	run docker image:
+		sudo docker run java_test
+
+output will be displayed in the console
